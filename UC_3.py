1ad3181db6051a270d19470786462a0c9712790f
vijay1 <vijaykumarynh7@gmail.com>
2024-07-22 18:13:07 +0530

    part time

---
+++ UC_3.py
@@ -0,0 +1,21 @@
+import random
+
+def main():
+    wage_per_hour = 20
+    full_day_hours = 8
+    half_day_hours = 4
+    print("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======\n")
+
+    emp_check = random.randint(0, 2)  # Generates 0, 1, or 2
+    if emp_check == 0:
+        print("Employee is Present")
+        print(f"The daily wage of Employee is: {wage_per_hour * full_day_hours}\n")
+    elif emp_check == 1:
+        print("Employee is Present but working part time")
+        print(f"The daily wage of Employee is: {wage_per_hour * half_day_hours}\n")
+    else:
+        print("Employee is Absent")
+        print("The daily wage of Employee is: 0\n")
+
+if __name__ == "__main__":
+    main()
